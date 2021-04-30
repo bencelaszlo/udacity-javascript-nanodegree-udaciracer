8720a3854815a5cb8ce3be01581fade4e0941155
bencelaszlo <bencelaszlo@protonmail.com>
2021-04-30 07:20:28 +0200

    Install eslint. Run eslint.
    On branch main
    
    Changes to be committed:
    	new file:   .eslintrc.js
    	modified:   data.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/client/assets/javascript/index.js
    	modified:   src/server/index.js

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -1,181 +1,179 @@
 // The store will hold all information needed globally
-var store = {
-	track_id: undefined,
-	player_id: undefined,
-	race_id: undefined,
+const store = {
+  track_id: undefined,
+  player_id: undefined,
+  race_id: undefined
 }
 
 // We need our javascript to wait until the DOM is loaded
-document.addEventListener("DOMContentLoaded", function() {
-	onPageLoad()
-	setupClickHandlers()
+document.addEventListener('DOMContentLoaded', function () {
+  onPageLoad()
+  setupClickHandlers()
 })
 
-async function onPageLoad() {
-	try {
-		getTracks()
-			.then(tracks => {
-				const html = renderTrackCards(tracks)
-				renderAt('#tracks', html)
-			})
+async function onPageLoad () {
+  try {
+    getTracks()
+      .then(tracks => {
+        const html = renderTrackCards(tracks)
+        renderAt('#tracks', html)
+      })
 
-		getRacers()
-			.then((racers) => {
-				const html = renderRacerCars(racers)
-				renderAt('#racers', html)
-			})
-	} catch(error) {
-		console.log("Problem getting tracks and racers ::", error.message)
-		console.error(error)
-	}
+    getRacers()
+      .then((racers) => {
+        const html = renderRacerCars(racers)
+        renderAt('#racers', html)
+      })
+  } catch (error) {
+    console.log('Problem getting tracks and racers ::', error.message)
+    console.error(error)
+  }
 }
 
-function setupClickHandlers() {
-	document.addEventListener('click', async function(event) {
-		const { target } = event
+function setupClickHandlers () {
+  document.addEventListener('click', async function (event) {
+    const { target } = event
 
-		// Race track form field
-		if (target.matches('.card.track')) {
-			handleSelectTrack(target)
-		}
+    // Race track form field
+    if (target.matches('.card.track')) {
+      handleSelectTrack(target)
+    }
 
-		// Podracer form field
-		if (target.matches('.card.podracer')) {
-			handleSelectPodRacer(target)
-		}
+    // Podracer form field
+    if (target.matches('.card.podracer')) {
+      handleSelectPodRacer(target)
+    }
 
-		// Submit create race form
-		if (target.matches('#submit-create-race')) {
-			event.preventDefault()
-	
-			handleCreateRace()
-		}
+    // Submit create race form
+    if (target.matches('#submit-create-race')) {
+      event.preventDefault()
 
-		// Handle acceleration click
-		if (target.matches('#gas-peddle')) {
-			await handleAccelerate(target)
-		}
+      handleCreateRace()
+    }
 
-	}, false)
+    // Handle acceleration click
+    if (target.matches('#gas-peddle')) {
+      await handleAccelerate(target)
+    }
+  }, false)
 }
 
-async function delay(ms) {
-	try {
-		return await new Promise(resolve => setTimeout(resolve, ms));
-	} catch(error) {
-		console.log("an error shouldn't be possible here")
-		console.log(error)
-	}
+async function delay (ms) {
+  try {
+    return await new Promise(resolve => setTimeout(resolve, ms))
+  } catch (error) {
+    console.log("an error shouldn't be possible here")
+    console.log(error)
+  }
 }
 
-async function handleCreateRace() {
-	const tracks = await getTracks();
-	const selectedTrack = tracks.find(track => track.id === parseInt(store.track_id))
-	const racers = await getRacers();
-	renderAt('#race', renderRaceStartView(selectedTrack, racers));
-	
-	const { player_id, track_id } = store
-	const race = await createRace(player_id, track_id)
-	// https://github.com/udacity/nd032-c3-asynchronous-programming-with-javascript-project-starter/issues/6
-	store.race_id = race.ID - 1;
+async function handleCreateRace () {
+  const tracks = await getTracks()
+  const selectedTrack = tracks.find(track => track.id === parseInt(store.track_id))
+  const racers = await getRacers()
+  renderAt('#race', renderRaceStartView(selectedTrack, racers))
 
-	await runCountdown();
-	await startRace(store.race_id);
-	await runRace(store.race_id);
+  const { player_id, track_id } = store
+  const race = await createRace(player_id, track_id)
+  // https://github.com/udacity/nd032-c3-asynchronous-programming-with-javascript-project-starter/issues/6
+  store.race_id = race.ID - 1
+
+  await runCountdown()
+  await startRace(store.race_id)
+  await runRace(store.race_id)
 }
 
-function runRace(raceID) {
-	return new Promise(resolve => {
-	const raceInterval = setInterval(async function() {
-		const race = await getRace(raceID)
+function runRace (raceID) {
+  return new Promise(resolve => {
+    const raceInterval = setInterval(async function () {
+      const race = await getRace(raceID)
 
-		if (race.status === "in-progress") {
-			renderAt('#leaderBoard', raceProgress(race.positions))
-		} else if (race.status === "finished") {
-			clearInterval(raceInterval)
-			renderAt('#race', resultsView(race.positions));
-			Promise.resolve(race.Results)
-		}
-	}, 500)
-	})
+      if (race.status === 'in-progress') {
+        renderAt('#leaderBoard', raceProgress(race.positions))
+      } else if (race.status === 'finished') {
+        clearInterval(raceInterval)
+        renderAt('#race', resultsView(race.positions))
+        Promise.resolve(race.Results)
+      }
+    }, 500)
+  })
 }
 
-async function runCountdown() {
-	try {
-		// wait for the DOM to load
-		await delay(1000)
-		let timer = 3
+async function runCountdown () {
+  try {
+    // wait for the DOM to load
+    await delay(1000)
+    let timer = 3
 
-		return new Promise(resolve => {
-			const timerInterval = setInterval(function() {
-				document.getElementById('big-numbers').innerHTML = --timer
+    return new Promise(resolve => {
+      const timerInterval = setInterval(function () {
+        document.getElementById('big-numbers').innerHTML = --timer
 
-				if (timer === 0) {
-					clearInterval(timerInterval);
-					resolve();
-				}
-
-			}, 1000);
-		})
-	} catch(error) {
-		console.log(error);
-	}
+        if (timer === 0) {
+          clearInterval(timerInterval)
+          resolve()
+        }
+      }, 1000)
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-function handleSelectPodRacer(target) {
-	// remove class selected from all racer options
-	const selected = document.querySelector('#racers .selected')
-	if(selected) {
-		selected.classList.remove('selected')
-	}
+function handleSelectPodRacer (target) {
+  // remove class selected from all racer options
+  const selected = document.querySelector('#racers .selected')
+  if (selected) {
+    selected.classList.remove('selected')
+  }
 
-	// add class selected to current target
-	target.classList.add('selected')
+  // add class selected to current target
+  target.classList.add('selected')
 
-	// save the selected racer to the store
-	store.player_id = target.id
+  // save the selected racer to the store
+  store.player_id = target.id
 }
 
-function handleSelectTrack(target) {
-	// remove class selected from all track options
-	const selected = document.querySelector('#tracks .selected')
-	if (selected) {
-		selected.classList.remove('selected')
-	}
+function handleSelectTrack (target) {
+  // remove class selected from all track options
+  const selected = document.querySelector('#tracks .selected')
+  if (selected) {
+    selected.classList.remove('selected')
+  }
 
-	// add class selected to current target
-	target.classList.add('selected')
+  // add class selected to current target
+  target.classList.add('selected')
 
-	// save the selected track id to the store
-	store.track_id = target.id
+  // save the selected track id to the store
+  store.track_id = target.id
 }
 
-async function handleAccelerate() {
-	await accelerate(store.race_id)
+async function handleAccelerate () {
+  await accelerate(store.race_id)
 }
 
 // HTML VIEWS ------------------------------------------------
 
-function renderRacerCars(racers) {
-	if (!racers.length) {
-		return `
+function renderRacerCars (racers) {
+  if (!racers.length) {
+    return `
 			<h4>Loading Racers...</4>
 		`
-	}
+  }
 
-	const results = racers.map(renderRacerCard).join('')
+  const results = racers.map(renderRacerCard).join('')
 
-	return `
+  return `
 		<ul id="racers">
 			${results}
 		</ul>
 	`
 }
 
-function renderRacerCard(racer) {
-	const { id, driver_name, top_speed, acceleration, handling } = racer
+function renderRacerCard (racer) {
+  const { id, driver_name, top_speed, acceleration, handling } = racer
 
-	return `
+  return `
 		<li class="card podracer" id="${id}">
 			<h3>${driver_name}</h3>
 			<p>${top_speed}</p>
@@ -185,41 +183,41 @@ function renderRacerCard(racer) {
 	`
 }
 
-function renderTrackCards(tracks) {
-	if (!tracks.length) {
-		return `
+function renderTrackCards (tracks) {
+  if (!tracks.length) {
+    return `
 			<h4>Loading Tracks...</4>
 		`
-	}
+  }
 
-	const results = tracks.map(renderTrackCard).join('')
+  const results = tracks.map(renderTrackCard).join('')
 
-	return `
+  return `
 		<ul id="tracks">
 			${results}
 		</ul>
 	`
 }
 
-function renderTrackCard(track) {
-	const { id, name } = track
+function renderTrackCard (track) {
+  const { id, name } = track
 
-	return `
+  return `
 		<li id="${id}" class="card track">
 			<h3>${name}</h3>
 		</li>
 	`
 }
 
-function renderCountdown(count) {
-	return `
+function renderCountdown (count) {
+  return `
 		<h2>Race Starts In...</h2>
 		<p id="big-numbers">${count}</p>
 	`
 }
 
-function renderRaceStartView(track, racers) {
-	return `
+function renderRaceStartView (track, racers) {
+  return `
 		<header>
 			<h1>Race: ${track.name}</h1>
 		</header>
@@ -238,10 +236,10 @@ function renderRaceStartView(track, racers) {
 	`
 }
 
-function resultsView(positions) {
-	positions.sort((a, b) => (a.final_position > b.final_position) ? 1 : -1)
+function resultsView (positions) {
+  positions.sort((a, b) => (a.final_position > b.final_position) ? 1 : -1)
 
-	return `
+  return `
 		<header>
 			<h1>Race Results</h1>
 		</header>
@@ -252,24 +250,24 @@ function resultsView(positions) {
 	`
 }
 
-function raceProgress(positions) {
-	let userPlayer = positions.find(e => e.id === parseInt(store.player_id))
-	userPlayer.driver_name += " (you)"
+function raceProgress (positions) {
+  const userPlayer = positions.find(e => e.id === parseInt(store.player_id))
+  userPlayer.driver_name += ' (you)'
 
-	positions = positions.sort((a, b) => (a.segment > b.segment) ? -1 : 1)
-	let count = 1
+  positions = positions.sort((a, b) => (a.segment > b.segment) ? -1 : 1)
+  let count = 1
 
-	const results = positions.map(p => {
-		return `
+  const results = positions.map(p => {
+    return `
 			<tr>
 				<td>
 					<h3>${count++} - ${p.driver_name}</h3>
 				</td>
 			</tr>
 		`
-	})
+  })
 
-	return `
+  return `
 		<main>
 			<h3>Leaderboard</h3>
 			<section id="leaderBoard">
@@ -279,86 +277,86 @@ function raceProgress(positions) {
 	`
 }
 
-function renderAt(element, html) {
-	const node = document.querySelector(element)
+function renderAt (element, html) {
+  const node = document.querySelector(element)
 
-	node.innerHTML = html
+  node.innerHTML = html
 }
 
 // API CALLS ------------------------------------------------
 
 const SERVER = 'http://localhost:8000'
 
-function defaultFetchOpts() {
-	return {
-		mode: 'cors',
-		headers: {
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin' : SERVER,
-		},
-	}
+function defaultFetchOpts () {
+  return {
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': SERVER
+    }
+  }
 }
 
-async function getTracks() {
-	try {
-		return await fetch(`${SERVER}/api/tracks`).then(res => res.json());
-	} catch (err) {
-		console.log("Problem with getTracks request::", err)
-	}
+async function getTracks () {
+  try {
+    return await fetch(`${SERVER}/api/tracks`).then(res => res.json())
+  } catch (err) {
+    console.log('Problem with getTracks request::', err)
+  }
 }
 
-async function getRacers() {
-	try {
-		return await fetch(`${SERVER}/api/cars`).then(res => res.json());
-	} catch (err) {
-		console.log("Problem with getRacers request::", err)
-	}
+async function getRacers () {
+  try {
+    return await fetch(`${SERVER}/api/cars`).then(res => res.json())
+  } catch (err) {
+    console.log('Problem with getRacers request::', err)
+  }
 }
 
-async function createRace(player_id, track_id) {
-	player_id = parseInt(player_id)
-	track_id = parseInt(track_id)
-	const body = { player_id, track_id }
+async function createRace (player_id, track_id) {
+  player_id = parseInt(player_id)
+  track_id = parseInt(track_id)
+  const body = { player_id, track_id }
 
-	try {
-		return await fetch(`${SERVER}/api/races`, {
-			method: 'POST',
-			...defaultFetchOpts(),
-			dataType: 'jsonp',
-			body: JSON.stringify(body)
-		}).then(res => res.json())
-	} catch (err) {
-		console.log("Problem with createRace request::", err)
-	}
+  try {
+    return await fetch(`${SERVER}/api/races`, {
+      method: 'POST',
+      ...defaultFetchOpts(),
+      dataType: 'jsonp',
+      body: JSON.stringify(body)
+    }).then(res => res.json())
+  } catch (err) {
+    console.log('Problem with createRace request::', err)
+  }
 }
 
-async function getRace(id) {
-	try {
-		const res = await fetch(`${SERVER}/api/races/${id}`)
-		return await res.json()
-	} catch (err) {
-		return console.log("Problem with getRace request::", err)
-	}
+async function getRace (id) {
+  try {
+    const res = await fetch(`${SERVER}/api/races/${id}`)
+    return await res.json()
+  } catch (err) {
+    return console.log('Problem with getRace request::', err)
+  }
 }
 
-async function startRace(id) {
-	try {
-		return fetch(`${SERVER}/api/races/${id}/start`, {
-			method: 'POST',
-			...defaultFetchOpts(),
-		})
-	} catch (err) {
-		return console.log("Problem with startRace request::", err)
-	}
+async function startRace (id) {
+  try {
+    return fetch(`${SERVER}/api/races/${id}/start`, {
+      method: 'POST',
+      ...defaultFetchOpts()
+    })
+  } catch (err) {
+    return console.log('Problem with startRace request::', err)
+  }
 }
 
-async function accelerate(id) {
-	try {
-		return await fetch(`${SERVER}/api/races/${id}/accelerate`, {
-			method: 'POST',
-			...defaultFetchOpts()
-		})
-	} catch (err) {
-		return console.log("Problem with accelerate request::", err)
-	}
+async function accelerate (id) {
+  try {
+    return await fetch(`${SERVER}/api/races/${id}/accelerate`, {
+      method: 'POST',
+      ...defaultFetchOpts()
+    })
+  } catch (err) {
+    return console.log('Problem with accelerate request::', err)
+  }
 }
